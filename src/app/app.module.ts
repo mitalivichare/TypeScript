f77265675b9d0493e677274536d4deafcf94a9d3
mitalivichare <vicharemitali26@gmail.com>
2018-01-10 15:13:17 +0530

    Project Up till Services

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import {FormsModule} from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import {HeroService} from './hero.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+     FormsModule
+  ],
+  providers: [HeroService, MessageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[]=[
+    { id : 11, name : 'Hrithik'},
+    { id : 12, name : 'Brad'},
+    { id : 13, name : 'Akshay'},
+    { id : 14, name : 'Salman'},
+    { id : 15, name : 'Varun'}
+];
